df5b9428337c1d513fb9f2072f2bfc1107c77db4
Julio Montaño <jmz7v.com@gmail.com>
2019-04-29 16:41:44 -0500

    [LIBS-47] Import demo data into main app component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React from 'react';
 
 // Components
 import LoanList from './components/LoanList';
+import loanData from './data/index.js';
 
 // Other
 import './App.css';
 
-function App() {
-  return (
-    <LoanList />
-  );
+class App extends React.Component {
+  render () {
+    console.log(loanData)
+    return (
+      <LoanList />
+    );
+  }
 }
 
 export default App;
