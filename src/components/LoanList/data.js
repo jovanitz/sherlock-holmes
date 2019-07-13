2a16b71fa5adf64e5a675103d01a2111d1c0c916
Joshua Torres Zapata <josh@iMac-de-Joshua.local>
2019-07-13 18:50:11 -0500

    add loadList component and styles

---
+++ src/components/LoanList/data.js
@@ -0,0 +1,30 @@
+export const TITLE_LIST = 'Listado de  Préstamos';
+
+export const headers = [ 'Cliente', 'Préstamo', 'Fecha', 'Estado' ];
+
+export const loanListData = [
+  {
+    customer: { name: 'Devora Kuzio', description: 'CJ00A1'},
+    loan: { amount : '$920,000.00', percentage: '18.5%' },
+    date: '9-abr-2018',
+    status: 1,
+  },
+  {
+    customer: { name: 'Devora Kuzio', description: 'CJ00A1'},
+    loan: { amount : '$920,000.00', percentage: '18.5%' },
+    date: '9-abr-2018',
+    status: 2,
+  },
+  {
+    customer: { name: 'Devora Kuzio', description: 'CJ00A1'},
+    loan: { amount : '$920,000.00', percentage: '18.5%' },
+    date: '9-abr-2018',
+    status: 3,
+  },
+  {
+    customer: { name: 'Devora Kuzio', description: 'CJ00A1'},
+    loan: { amount : '$920,000.00', percentage: '18.5%' },
+    date: '9-abr-2018',
+    status: 0,
+  },
+];
--- src/components/LoanList/index.js
+++ src/components/LoanList/index.js
@@ -1,9 +1,53 @@
 // Libraries
-import React from 'react'
+import React from 'react';
+import { TITLE_LIST, headers, loanListData } from './data';
+import './LoanList.scss';
 
-const LoanList = ({}) => (
+const renderStatus = status => {
+  switch(status) {
+    case 1: return <div className="LoanList-status LoanList-status--green">A TIEMPO</div>
+    case 2: return <div className="LoanList-status LoanList-status--yellow">RETRASO 1-30</div>
+    case 3: return <div className="LoanList-status LoanList-status--orange">RETRASO 31-60</div>
+    default: return <div className="LoanList-status LoanList-status--red">DEFAULT</div>
+  }
+}
+
+const renderHeaders = () => headers.map((name, index) => {
+  const classIsRight = index % 2 ? 'align-right' : '';
+
+  return <div className={ `LoanList-header__item ${ classIsRight }` }>{ name }</div>
+});
+
+const renderLoanlist = () => loanListData.map(loanData => {
+  const { customer, loan, date, status } = loanData;
+  const { name, description } = customer;
+  const { amount, percentage } = loan;
+
+  return (
+    <div className="LoanList__content">
+      <div className="LoanList-content-item">
+        <div className="LoanList-content-item__name">{ name }</div>
+        <div className="LoanList-content-item__description">{ description }</div>
+      </div>
+      <div className="LoanList-content-item align-right">
+        <div className="LoanList-content-item__name">{ amount }</div>
+        <div className="LoanList-content-item__description">{ percentage }</div>
+      </div>
+      <div className="LoanList-content-item">
+        <div className="LoanList-content-item__name">{ date }</div>
+      </div>
+      <div className="LoanList-content-item align-right">{ renderStatus(status) }</div>
+    </div>
+  )
+});
+
+const LoanList = () => (
   <React.Fragment>
-    Listado de Loans
+    <div className="LoanList">
+      <div className="LoanList__title">{ TITLE_LIST }</div>
+      <div className="LoanList__header">{ renderHeaders() }</div>
+      { renderLoanlist() }
+    </div>
   </React.Fragment>
 )
 
